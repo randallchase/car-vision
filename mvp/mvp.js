6e16f1a4340a30e193a307ef3a104d49f75b0eef
Randall M <rmarkham@mit.edu>
2018-11-24 19:51:46 -0600

    added basic API call

--- mvp/mvp.js
+++ mvp/mvp.js
@@ -1,36 +1,40 @@
 var express = require('express');
 var app     = express();
+var request = require("request");
+var fs      = require('fs');
+var credentialsJSON = JSON.parse(fs.readFileSync('./credentials.json', 'utf8'));
+
+// setup directory used to serve static files
+app.use(express.static('public'));
+
 var cors = require('cors');
 app.use(cors());
-var fs = require('fs');
-var credentials_JSON = JSON.parse(fs.readFileSync('../config.json', 'utf8'));
-
-// Read in API key from JSON to prevent storing credentials in GitHub
-var algo_key = credentials_JSON['algorithmia-key'];
 
 
+var image = fs.createReadStream('./project_images/Motor-Trend-group-photo.jpg');
 
-var Algorithmia = require("algorithmia");
+var options = { method: 'POST',
+    url: 'https://dev.sighthoundapi.com/v1/recognition',
+    qs: { objectType: 'vehicle' },
+    headers:
+        {
+            'Content-Type': 'application/octet-stream',
+            'X-Access-Token': credentialsJSON['sighthound-key']
+        },
+    body: image};
 
-var input = "https://st.motortrend.com/uploads/sites/10/2015/09/2016-Honda-Accord-Touring-MT-promo.jpg";
-Algorithmia.client(algo_key)
-    .algo("LgoBE/CarMakeandModelRecognition/0.3.15")
-    .pipe(input)
-    .then(function(response) {
-        console.log(response.get());
-    });
+var results = request(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-/*
-var input = {
-   "input_file":"data://media/videos/kenny_test.mp4",
-   "output_file":"data://.algo/temp/kenny_test_nudity.json",
-   "algorithm":"algo://LgoBE/CarMakeandModelRecognition/0.3.15",
-   "fps": 1
-};
-Algorithmia.client(algo_key)
-    .algo("media/VideoMetadataExtraction/0.6.0")
-    .pipe(input)
-    .then(function(response) {
-        console.log(response.get());
-    });
-*/
+    console.log(body);
+});
+
+resultObj = JSON.parse(results.response.body)
+
+for (var i = 0; i < resultObj.objects, i++) {
+
+}
+
+// resultObj.objects[0].vehicleAnnotation.bounding.vertices
+// resultObj.objects[0].vehicleAnnotation.recognitionConfidence
+// resultObj.objects[0].vehicleAnnotation.attributes.system. // make // model // color // vehicleType

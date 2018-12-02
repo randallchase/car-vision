c7071efe51de768768baef4b1e3272d3bb245ccf
Randall M <rmarkham@mit.edu>
2018-12-02 16:06:08 -0500

    updated sql

--- app-public/car-vis-dev.sql
+++ app-public/car-vis-dev.sql
@@ -13,6 +13,38 @@ CREATE TABLE cars_pass (
     confidence DECIMAL
 );
 
+
+CREATE TABLE image (
+    id SERIAL PRIMARY KEY,
+    dtg TIMESTAMP,
+    url VARCHAR
+);
+
+
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    make VARCHAR,
+    car_model VARCHAR,
+    color VARCHAR,
+    veh_type VARCHAR,
+    ad_category integer
+);
+
+
+CREATE TABLE image_has_cars (
+    image_id INTEGER REFERENCES image(id),
+    car_id INTEGER REFERENCES cars(id),
+    confidence DECIMAL,
+    PRIMARY KEY (image_id, car_id)
+);
+
+
+CREATE FUNCTION cars_and_images()
+    RETURNS TRIGGER
+AS $$
+BEGIN
+    INSERT INTO image_has_cars (image_id) VALUES
+
 INSERT INTO cars_pass (image_id, make, car_model, color, veh_type, confidence)
     VALUES (1, 'BMW', '2 Series', 'blue', 'car', 0.2728),
             (1, 'Ford', 'Mustang', 'red', 'car', 0.9822),
@@ -23,4 +55,5 @@ INSERT INTO cars_pass (image_id, make, car_model, color, veh_type, confidence)
             (1, 'Honda', 'Accord', 'red', 'car', 0.3162),
             (1, 'Chevrolet', 'Camaro', 'green', 'car', 0.675),
             (1, 'Mazda', 'CX-3', 'black', 'suv', 0.138),
-            (1, 'Chevrolet', 'Bolt', 'orange', 'car', 0.9694);
+            (1, 'Chevrolet', 'Bolt', 'orange', 'car', 0.9694),
+            (2, 'Honda', 'Accord', 'black', 'car', 0.6757);
